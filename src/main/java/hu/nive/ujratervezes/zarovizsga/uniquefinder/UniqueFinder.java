6a9684facc2a7649713b0e35e95666ecd2e6dd8b
pgk <pgk977@outlook.com>
2021-04-23 07:38:27 +0200

    1

--- src/main/java/hu/nive/ujratervezes/zarovizsga/uniquefinder/UniqueFinder.java
+++ src/main/java/hu/nive/ujratervezes/zarovizsga/uniquefinder/UniqueFinder.java
@@ -1,6 +1,24 @@
 package hu.nive.ujratervezes.zarovizsga.uniquefinder;
 
+import java.util.*;
+
 public class UniqueFinder {
 
+    public List<Character> uniqueChars (String input){
+        if(input == null){
+            throw new IllegalArgumentException();
 
+        }
+        List<Character> result = new ArrayList<>();
+        Map<Character, Integer> chars = new LinkedHashMap<>();
+
+        for(int i=0; i<input.length(); i++) {
+            chars.put(input.charAt(i),1);
+        }
+
+        for(Map.Entry<Character,Integer> chr: chars.entrySet() ){
+            result.add(chr.getKey());
+        }
+            return result;
+    }
 }
